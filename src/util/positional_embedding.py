8c81b37462371a6a4aca12a9da4e2f2c764a36f5
Billy Li <li000400@umn.edu>
2024-09-01 21:23:22 +0200

    dev: pos emb

--- src/util/positional_embedding.py
+++ src/util/positional_embedding.py
@@ -3,21 +3,20 @@ import torch
 def get_1d_sincos_pos_emb(embed_dim, pos):
     """
     embed_dim: output dimension for each position
-    pos: a list of positions to be encoded: size (M,)
-    out: (M, D)
+    pos: a list of positions to be encoded: size (B,S,1)
+    out: (B, S, D)
     """
     assert embed_dim % 2 == 0
     omega = torch.arange(embed_dim // 2, dtype=float)
     omega /= embed_dim / 2.
     omega = 1. / 10000**omega   # (D/2,)
 
-    pos = pos.reshape(-1)   # (M,)
-    out = torch.einsum('m,d->md', pos, omega)   # (M, D/2), outer product
+    out = torch.einsum('bs,d->bsd', pos, omega)   # (B, S, D/2), outer product
 
-    emb_sin = torch.sin(out)  # (M, D/2)
-    emb_cos = torch.cos(out)  # (M, D/2)
+    emb_sin = torch.sin(out)  # (B, S, D/2)
+    emb_cos = torch.cos(out)  # (B, S, D/2)
 
-    emb = torch.cat([emb_sin, emb_cos], axis=1)  # (M, D)
+    emb = torch.cat([emb_sin, emb_cos], axis=2)  # (B, S, D)
     return emb
 
 
@@ -48,7 +47,8 @@ def create_pos_emb_fn(emb_dim):
         emb_eta = get_1d_sincos_pos_emb(emb_dim//2, sj_eta)
         emb_phi_star = get_1d_sincos_pos_emb(emb_dim//2, sj_phi_star)
 
-        emb = torch.cat([emb_eta, emb_phi_star], axis=1)
+        print(emb_phi_star.shape)
+        emb = torch.cat([emb_eta, emb_phi_star], axis=2)
 
         return emb
 
