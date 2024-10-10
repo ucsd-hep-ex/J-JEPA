6dc1e8a572625bcbd1e31dc409bf187813042ccc
JavierZhao <zhaoziha@usc.edu>
2024-10-10 13:42:59 -0700

    modified ParticleDataset to unsuqeeze mask

--- src/dataset/ParticleDataset.py
+++ src/dataset/ParticleDataset.py
@@ -6,8 +6,11 @@ import numpy as np
 from collections import namedtuple
 import gc
 
-DataSample = namedtuple('DataSample', ['p4_spatial', 'p4', 'mask'])
-DataSample_label = namedtuple('DataSample_label', ['p4_spatial', 'p4', 'mask', 'labels'])
+DataSample = namedtuple("DataSample", ["p4_spatial", "p4", "mask"])
+DataSample_label = namedtuple(
+    "DataSample_label", ["p4_spatial", "p4", "mask", "labels"]
+)
+
 
 class ParticleDataset(Dataset):
     def __init__(self, directory_path, num_jets=None, return_labels=False):
@@ -22,11 +25,13 @@ class ParticleDataset(Dataset):
         for filename in os.listdir(directory_path):
             if filename.endswith(".hdf5") or filename.endswith(".h5"):
                 file_path = os.path.join(directory_path, filename)
-                with h5py.File(file_path, 'r') as hdf:
+                with h5py.File(file_path, "r") as hdf:
                     print(f"Loading {directory_path}/{filename}")
-                    particles = {name: hdf["particles"][name][:] for name in hdf["particles"]}
-                    labels = hdf['labels'][:]
-                    mask = hdf['mask'][:]
+                    particles = {
+                        name: hdf["particles"][name][:] for name in hdf["particles"]
+                    }
+                    labels = hdf["labels"][:]
+                    mask = hdf["mask"][:]
                     stats = {name: hdf["stats"][name][:] for name in hdf["stats"]}
 
                 # Append data to lists/dicts
@@ -46,17 +51,33 @@ class ParticleDataset(Dataset):
                 print(f"Loaded {directory_path}/{filename}")
 
         # Concatenate all the data
-        self.particles = {key: np.concatenate(particles_dict[key], axis=0) for key in particles_dict}
+        self.particles = {
+            key: np.concatenate(particles_dict[key], axis=0) for key in particles_dict
+        }
         self.labels = np.concatenate(labels_list, axis=0)
         self.mask = np.concatenate(mask_list, axis=0)
-        self.stats = {key: np.concatenate(stats_dict[key], axis=0) for key in stats_dict}
+        self.stats = {
+            key: np.concatenate(stats_dict[key], axis=0) for key in stats_dict
+        }
 
-        part_features = particles_dict.keys() # [part_px, part_py, part_pz, part_deta, part_dphi, part_pt_log, part_e_log]
-        mean_log_e, std_log_e = self.stats['part_e_log']
+        part_features = (
+            particles_dict.keys()
+        )  # [part_px, part_py, part_pz, part_deta, part_dphi, part_pt_log, part_e_log]
+        mean_log_e, std_log_e = self.stats["part_e_log"]
         log_e = self.particles["part_e_log"] * std_log_e + mean_log_e
         norm_energy = np.exp(log_e) * self.mask
-        self.p4_spatial = np.stack([self.particles[key] for key in ["part_px", "part_py", "part_pz"]] + [norm_energy], axis=1) # for pos emb
-        self.p4 = np.stack([self.particles[key] for key in ["part_deta", "part_dphi", "part_pt_log", "part_e_log"]], axis=1) # for input to J-JEPA
+        self.p4_spatial = np.stack(
+            [self.particles[key] for key in ["part_px", "part_py", "part_pz"]]
+            + [norm_energy],
+            axis=1,
+        )  # for pos emb
+        self.p4 = np.stack(
+            [
+                self.particles[key]
+                for key in ["part_deta", "part_dphi", "part_pt_log", "part_e_log"]
+            ],
+            axis=1,
+        )  # for input to J-JEPA
 
         # Limit to num_jets if specified
         if num_jets:
@@ -67,9 +88,20 @@ class ParticleDataset(Dataset):
             for key in self.stats:
                 self.stats[key] = self.stats[key][:num_jets]
         if self.return_labels:
-            print("__getitem__ returns", ['p4_spatial (px, py, pz, e)', 'p4 (eta, phi, log_pt, log_e)', 'mask', 'labels'])
+            print(
+                "__getitem__ returns",
+                [
+                    "p4_spatial (px, py, pz, e)",
+                    "p4 (eta, phi, log_pt, log_e)",
+                    "mask",
+                    "labels",
+                ],
+            )
         else:
-            print("__getitem__ returns", ['p4_spatial (px, py, pz, e)', 'p4 (eta, phi, log_pt, log_e)', 'mask'])
+            print(
+                "__getitem__ returns",
+                ["p4_spatial (px, py, pz, e)", "p4 (eta, phi, log_pt, log_e)", "mask"],
+            )
         del self.particles
         gc.collect()
 
@@ -79,16 +111,16 @@ class ParticleDataset(Dataset):
     def __getitem__(self, idx):
         if self.return_labels:
             sample = DataSample_label(
-        p4_spatial = torch.from_numpy(self.p4_spatial[idx]),
-        p4 = torch.from_numpy(self.p4[idx]),
-        mask = torch.from_numpy(self.mask[idx]),
-        labels = self.labels[idx]
-    )
+                p4_spatial=torch.from_numpy(self.p4_spatial[idx]),
+                p4=torch.from_numpy(self.p4[idx]),
+                mask=torch.from_numpy(self.mask[idx]).unsqueeze(1),
+                labels=self.labels[idx],
+            )
         else:
             sample = DataSample(
-        p4_spatial = torch.from_numpy(self.p4_spatial[idx]),
-        p4 = torch.from_numpy(self.p4[idx]),
-        mask = torch.from_numpy(self.mask[idx]),
-    )
+                p4_spatial=torch.from_numpy(self.p4_spatial[idx]),
+                p4=torch.from_numpy(self.p4[idx]),
+                mask=torch.from_numpy(self.mask[idx]).unsqueeze(1),
+            )
 
         return sample
