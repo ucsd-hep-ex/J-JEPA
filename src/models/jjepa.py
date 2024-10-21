585b2a03d2536dfaa269fe6035daab0e4596f97f
JavierZhao <zhaoziha@usc.edu>
2024-10-21 13:26:06 -0700

    used JetPredictor instead

--- src/models/jjepa.py
+++ src/models/jjepa.py
@@ -336,9 +336,9 @@ class JJEPA(nn.Module):
 
         # if self.use_predictor:
         # if self.use_parT:
-        self.predictor_transformer = ParTPredictor(options=options)
+        # self.predictor_transformer = ParTPredictor(options=options)
         # else:
-        # self.predictor_transformer = JetsTransformerPredictor(options)
+        self.predictor_transformer = JetsTransformerPredictor(options)
 
         if self.options.debug:
             self.input_check = DimensionCheckLayer("Model Input", 3)
@@ -363,25 +363,25 @@ class JJEPA(nn.Module):
         )
 
         # if self.use_parT:
+        # context_repr = self.context_transformer(
+        #     full_jet["p4"],
+        #     full_jet["p4_spatial"],
+        #     full_jet["particle_mask"],
+        #     context_split_mask,
+        # )
+        # target_repr = self.target_transformer(
+        #     full_jet["p4"],
+        #     full_jet["p4_spatial"],
+        #     full_jet["particle_mask"],
+        #     target_split_mask,
+        # )
+        # else:
         context_repr = self.context_transformer(
-            full_jet["p4"],
-            full_jet["p4_spatial"],
-            full_jet["particle_mask"],
-            context_split_mask,
+            full_jet["p4"], full_jet["particle_mask"], context_split_mask
         )
         target_repr = self.target_transformer(
-            full_jet["p4"],
-            full_jet["p4_spatial"],
-            full_jet["particle_mask"],
-            target_split_mask,
+            full_jet["p4"], full_jet["particle_mask"], target_split_mask
         )
-        # else:
-        #     context_repr = self.context_transformer(
-        #         full_jet["p4"], full_jet["particle_mask"], context_split_mask
-        #     )
-        #     target_repr = self.target_transformer(
-        #         full_jet["p4"], full_jet["particle_mask"], target_split_mask
-        #     )
         if self.options.debug:
             print(f"Context repr shape: {context_repr.shape}")
             print(f"Target repr shape: {target_repr.shape}")
