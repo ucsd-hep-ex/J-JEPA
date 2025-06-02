9c1f02979326275eed4f63e18d782e3ca6874071
alanx1234 <alxia@ucsd.edu>
2025-06-01 17:15:13 -0700

    fix

--- src/models/train_model_ptcl.py
+++ src/models/train_model_ptcl.py
@@ -460,16 +460,6 @@ def main(rank, world_size, args):
             context_masks = context_masks.to(device)
             target_masks = target_masks.to(device)
 
-            context_masks_expanded = context_masks.unsqueeze(-1)  # Shape: [B, N, 1]
-            context_masks_expanded = context_masks_expanded.expand(
-                -1, -1, 4
-            )  # Shape: [B, N, 4]
-
-            target_masks_expanded = target_masks.unsqueeze(-1)  # Shape: [B, N, 1]
-            target_masks_expanded = target_masks_expanded.expand(
-                -1, -1, 4
-            )  # Shape: [B, N, 4]
-
                         # End Data Loading Timer
             end_data_loading = time.time()
             logger.info(f"Data loading time for batch {itr}: {end_data_loading - start_data_loading:.3f} seconds")
@@ -486,10 +476,10 @@ def main(rank, world_size, args):
                     B = p4_spatial.shape[0]
                     N_ctxt = context_masks.sum(dim=1).max().item()
                     N_trgt = target_masks.sum(dim=1).max().item()
-                    p4_context = p4[context_masks_expanded].view(B, N_ctxt, 4)
-                    p4_target = p4[target_masks_expanded].view(B, N_trgt, 4)
-                    ctxt_particle_mask = particle_mask[context_masks].view(B, N_ctxt)
-                    trgt_particle_mask = particle_mask[target_masks].view(B, N_trgt)
+                    p4_context = torch.masked_select(p4, context_masks.unsqueeze(-1)).view(B, N_ctxt, 4)        
+                    p4_target  = torch.masked_select(p4, target_masks .unsqueeze(-1)).view(B, N_trgt, 4)       
+                    ctxt_particle_mask = torch.masked_select(particle_mask, context_masks).view(B, N_ctxt)
+                    trgt_particle_mask = torch.masked_select(particle_mask, target_masks ).view(B, N_trgt)
 
                     context = {
                         "p4": p4_context,
